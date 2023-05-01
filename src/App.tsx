86b5fcfc088defbdb4a0d466a09da6ab587074ef
Valera <valerakaktotak@gmail.com>
2023-05-01 21:15:43 +0300

    installed reduxToolkit and react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <div className="font-bold">HELLO REACT :)</div>
+  return <div className="font-bold text-red-950 bg-blue-600">HELLO REACT :)</div>
 }
 
 export default App
