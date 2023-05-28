d78e9a063bc5b0474b98633c99f455674d758d57
Valera <valerakaktotak@gmail.com>
2023-05-28 21:17:25 +0300

    finished favourite page and corrected add and remove favourite items

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,18 +1,51 @@
+import { useState } from 'react'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
 import { UserRepo } from '../models/models'
 
 export function RepoCard({ repo }: { repo: UserRepo }) {
+  const { addFavourite, removeFavourite } = useActions()
+  const { favourites } = useAppSelector((state) => state.githubFavourite)
+  const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
+  const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    addFavourite(repo.html_url)
+    setIsFav(true)
+  }
+  const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    removeFavourite(repo.html_url)
+    setIsFav(false)
+  }
   return (
-    <a href={repo.html_url} target="_blank" rel="noreferrer">
+    <>
       <div className="border px-2 py-4 rounded mb-2 hover:bg-slate-400 hover:shadow-md transition-all">
-        <h2 className="text-lg font-bold">{repo.full_name}</h2>
-        <p className="text-sm">
-          Forks: <span className="font-bold mb-2">{repo.forks}</span>
-          Watchers: <span className="font-bold mb-2">{repo.watchers}</span>
-        </p>
-        <p className="text-sm font-thin">
-          <span>{repo?.description}</span>
-        </p>
+        <a href={repo.html_url} target="_blank" rel="noreferrer">
+          <h2 className="text-lg font-bold">{repo.full_name}</h2>
+          <p className="text-sm">
+            Forks: <span className="font-bold mb-2">{repo.forks}</span>
+            Watchers: <span className="font-bold mb-2">{repo.watchers}</span>
+          </p>
+          <p className="text-sm font-thin">
+            <span>{repo?.description}</span>
+          </p>
+        </a>
+        <div className="flex justify-between mt-4">
+          {!isFav && (
+            <button className="px-2 py-4 rounded bg-green-600 hover: shadow-md transition-all" onClick={addToFavourite}>
+              Add
+            </button>
+          )}
+          {isFav && (
+            <button
+              className="px-2 py-4 rounded bg-red-600 hover: shadow-md transition-all"
+              onClick={removeFromFavourite}
+            >
+              Remove
+            </button>
+          )}
+        </div>
       </div>
-    </a>
+    </>
   )
 }
--- src/pages/FavoritePages.tsx
+++ src/pages/FavoritePages.tsx
@@ -1,7 +1,24 @@
+import { useAppSelector } from '../hooks/redux'
+
 function FavoritePages() {
+  const { favourites } = useAppSelector((state) => state.githubFavourite)
+  if (favourites.length === 0) return <p className="text-center">No item...</p>
   return (
     <>
-      <div>favoritePages</div>
+      <div className="flex flex-col pt-10 mx-auto h-screen w-screen">
+        <ul className="list-none w-full max-w-xl relative mx-auto">
+          {favourites.map((item) => (
+            <li
+              className="border px-2 py-4 rounded mb-2 hover:bg-slate-400 hover:shadow-md transition-all cursor-pointer"
+              key={item}
+            >
+              <a href={item} target="_blank" rel="noreferrer">
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
