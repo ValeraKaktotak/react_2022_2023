62df95e267439440affe9b6c27ac4e6c24380c78
Valera <valerakaktotak@gmail.com>
2023-05-28 21:31:16 +0300

    added remove btn in fav page and DONE this learning project

--- src/pages/FavoritePages.tsx
+++ src/pages/FavoritePages.tsx
@@ -1,7 +1,12 @@
+import { useActions } from '../hooks/actions'
 import { useAppSelector } from '../hooks/redux'
 
 function FavoritePages() {
   const { favourites } = useAppSelector((state) => state.githubFavourite)
+  const { removeFavourite } = useActions()
+  const removeFromFavourite = (fav: string) => {
+    removeFavourite(fav)
+  }
   if (favourites.length === 0) return <p className="text-center">No item...</p>
   return (
     <>
@@ -9,12 +14,18 @@ function FavoritePages() {
         <ul className="list-none w-full max-w-xl relative mx-auto">
           {favourites.map((item) => (
             <li
-              className="border px-2 py-4 rounded mb-2 hover:bg-slate-400 hover:shadow-md transition-all cursor-pointer"
+              className="flex flex-col border px-2 py-4 rounded mb-2 hover:bg-slate-400 hover:shadow-md transition-all cursor-pointer"
               key={item}
             >
               <a href={item} target="_blank" rel="noreferrer">
                 {item}
               </a>
+              <button
+                className="px-2 py-4 rounded bg-red-600 hover: shadow-md transition-all"
+                onClick={() => removeFromFavourite(item)}
+              >
+                Remove
+              </button>
             </li>
           ))}
         </ul>
